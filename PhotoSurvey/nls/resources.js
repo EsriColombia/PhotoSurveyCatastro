69ed5e4021dae501d76c9a6327b48a1e1af13fc2
Rene Vasquez <rene.jassen@gmail.com>
2016-05-04 15:32:12 -0500

    Primer commit

---
+++ PhotoSurvey/nls/resources.js
@@ -0,0 +1,85 @@
+﻿/*global define */
+/** @license
+ | Copyright 2015 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
+define({
+    root: ({
+        screenreader: {  // Document structure for screen reader devices
+            h1_title: "Photo-based survey",
+            h2_splash: "Ingresar por redes sociales",
+            h2_illustration: "Galería de fotos",
+            h2_activity: "Encuesta y perfil",
+            h2_additionalInfo: "Información adicional"
+        },
+        tooltips: {
+            button_additionalInfo: "Información adicional",
+            button_close: "Cerrar panel de Información adicional",
+            flag_important_question: "Por favor conteste esta pregunta",
+            button_previous_image: "Imagen anterior",
+            button_next_image: "Siguiente imagen",
+            button_best_image: "Esta es la mejor foto de la propiedad",
+            button_click_if_best_image: "Clic si esta es la mejor foto de la propiedad",
+            button_skip: "Saltar",
+            button_submit: "Envíar encuesta"
+        },
+        labels: {
+            menuItem_profile: "Perfil",
+            menuItem_signout: "Salir",
+            button_close: "Cerrar",
+            label_surveys_completed: "encuestas completadas",
+            label_level: "nivel ${0}",
+            label_remaining_surveys: "${0} encuestas para el próximo nivel",
+            button_returnToSurvey: "&lt; Volver a la encuesta"
+        },
+        signin: {
+            checkingServer: "Revisando la disponibiladad del servidor...",
+            unsupported: "Esta versión del navegador no es compatible. Por favor actualice su navegador",
+            needProxy: "Esta versión del navegador no está soportado por nuestro servidor. Por favor actualice su navegador.",
+            signinFetching: "Revisando la disponibiladad para los tipos de ingreso...",
+            signinLoginPrompt: "Por favor ingrese para empezar",
+            noMoreSurveys: "No hay encuestas disponibles en este momento. Gracias por participar",
+            guestLabel: "Invitado"
+        }
+    }),
+    "ar": 0,
+    "cs": 0,
+    "da": 0,
+    "de": 1,
+    "el": 0,
+    "es": 0,
+    "et": 0,
+    "fi": 0,
+    "fr": 1,
+    "he": 0,
+    "it": 0,
+    "ja": 0,
+    "ko": 0,
+    "lt": 0,
+    "lv": 0,
+    "nb": 0,
+    "nl": 0,
+    "pl": 0,
+    "pt-br": 0,
+    "pt-pt": 0,
+    "ro": 0,
+    "ru": 0,
+    "sv": 0,
+    "th": 0,
+    "tr": 0,
+    "vi": 0,
+    "zh-cn": 0,
+    "zh-hk": 0,
+    "zh-tw": 0
+});
